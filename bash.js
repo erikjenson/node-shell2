415a5fceabcbef5619d5819c6067b33d9c155594
Ziv <zkarmi25@gmail.com>
2019-11-11 13:08:33 -0500

    Added cat

--- bash.js
+++ bash.js
@@ -1,10 +1,20 @@
 const pwd = require('./pwd.js');
+const ls = require('./ls.js');
+const cat = require('./cat.js');
 
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', data => {
-  const cmd = data.toString().trim();
-  if (cmd === 'pwd') {
-    pwd();
+  const cmd = data.toString().trim().split(' ');
+  switch(cmd[0]) {
+    case 'pwd':
+      pwd();
+      break;
+    case 'ls':
+      ls();
+      break;
+    case 'cat':
+      cat(cmd[1]);
+      break;
   }
 });
